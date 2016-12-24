02e1c05c36d9f0f10257a628b6d9ae4fbcea2573
Michael Bykov <m.bykov@gmail.com>
2016-12-24 19:57:55 +0300

    i

--- contents.js
+++ contents.js
@@ -4,6 +4,7 @@
 
 // let antrax = document.getElementById('antrax-result')
 const antrax = require('./antrax')
+const _ = require('underscore')
 
 
 /* δηλοῖ δέ μοι καὶ τόδε τῶν παλαιῶν ἀσθένειαν οὐχ ἤκιστα. πρὸ γὰρ τῶν Τρωικῶν οὐδὲν φαίνεται πρότερον κοινῇ ἐργασαμένη ἡ Ἑλλάς. δοκεῖ δέ μοι, οὐδὲ τοὄνομα τοῦτο ξύμπασά πω εἶχεν, ἀλλὰ τὰ μὲν πρὸ Ἕλληνος τοῦ Δευκαλίωνος καὶ πάνυ οὐδὲ εἶναι ἡ ἐπίκλησις αὕτη. κατὰ ἔθνη δὲ ἄλλα τε καὶ τὸ Πελασγικὸν ἐπὶ πλεῖστον ἀφ' ἑαυτῶν τὴν ἐπωνυμίαν παρέχεσθαι. Ἕλληνος δὲ καὶ τῶν παίδων αὐτοῦ ἐν τῇ Φθιῶτιδι ἰσχυσάντων, καὶ ἐπαγομένων αὐτοὺς ἐπ' ὠφελίᾳ ἐς τὰς ἄλλας πόλεις, καθ' ἑκάστους μὲν ἤδη τῇ ὁμιλίᾳ μᾶλλον καλεῖσθαι Ἕλληνας. οὐ μέντοι πολλοῦ γε χρόνου ἐδύνατο καὶ ἅπασιν ἐκνικῆσαι. τεκμηριοῖ δὲ μάλιστα Ὅμερος πολλῷ γὰρ ὕστερον ἔτι καὶ τῶν Τρωικῶν γενόμενος οὐδαμοῦ οὕτω τοὺς ξύμπαντας ὠνόμασεν οὐδ' ἄλλους ἢ τοὺς μετὰ Ἀχιλλέως ἐκ τῆς Φθιώτιδος. οἵπερ καὶ πρῶτοι Ἕλληνες ἧσαν, Δαναοὺς δὲ ἐν τοῖς ἔπεσι καὶ Ἀργείους καὶ Ἀχαιοὺς ἀνακαλεῖ. οὐ μὴν οὐδὲ βαρβάρους εἴρηκε διὰ τὸ μηδὲ Ἕλληνάς πω, ὡς ἐμοὶ δοκεῖ. ἀντίπαλον ἐς ἓν ὄνομα ἀποκεκρίσθαι. οἱ δ' οὖν ὡς ἕκαστοι Ἕλληνες κατὰ πόλεις τε ὅσοι ἀλλήλων ξυνίεσαν καὶ ξύμπαντες ὕστερον κληθέντες οὐδὲν πρὸ τῶν Τρωικῶν δι' ἀσθένειαν καὶ ἀμειξίαν ἀλλήλων ἁθρόοι ἔπραξαν. ἀλλὰ καὶ ταύτην τὴν στρατείαν θαλάσσῃ ἤδη πλείω χρώμενοι ξυνῆλθον.
@@ -12,7 +13,7 @@ const antrax = require('./antrax')
 require('electron').ipcRenderer.on('ping', (event, json) => {
     let oRes = document.getElementById('antrax-result')
     let obj = JSON.parse(json)
-    log('OBJ', obj)
+    log('MSG', obj)
     antrax.query(obj.sentence, obj.num, function(clause) {
         log('popup:', clause)
         oRes.textContent = obj.sentence
@@ -20,8 +21,9 @@ require('electron').ipcRenderer.on('ping', (event, json) => {
 })
 
 
-function drawHeader(sentence) {
-
+function drawHeader(clause) {
+    let oHeader = document.getElementById('antrax-header')
+    let keys = _.keys(clause)
 }
 
 function xxx(sentence) {
@@ -30,5 +32,56 @@ function xxx(sentence) {
 
 
 
+function q(sel) {
+    return document.querySelector(sel);
+}
+
+function qs(sel) {
+    return document.querySelectorAll(sel);
+}
+
+function cre(tag) {
+    return document.createElement(tag);
+}
+
+function cret(str) {
+    return document.createTextNode(str);
+}
+
+function sa(str) {
+    var oSa = cre('span');
+    classes(oSa).add('nagari');
+    oSa.textContent = str;
+    return oSa;
+}
+
+function empty(el) {
+    while (el.hasChildNodes()) {
+        el.removeChild(el.lastChild);
+    }
+}
+
+function remove(el) {
+    el.parentElement.removeChild(el);
+}
+
+function closeAll() {
+    // var popups = qs('.morph-popup');
+    // var arr = [].slice.call(popups);
+    // arr.forEach(function(popup) {
+    //     popup.parentElement.removeChild(popup);
+    // });
+    // var oTip = q('#tip');
+    // if (oTip) oTip.parentElement.removeChild(oTip);
+}
+
+document.onkeyup = function(e) {
+    if (e.which === 27) { //Esc
+        // closeAll();
+        // <a id="close" href="javascript:window.close()">Close this Window</a>
+        // window.close()
+        log('ESCAPE')
+    }
+}
 
 function log() { console.log.apply(console, arguments); }
