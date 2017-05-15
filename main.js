401b365e61c0dc28e404464b0d8cdd29d27e431b
Michael Bykov <m.bykov@gmail.com>
2017-05-15 21:56:47 +0300

    i

--- main.js
+++ main.js
@@ -67,6 +67,7 @@ function createWindow(msg) {
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
         clearInterval(timerId)
+        tray = null
         timerId = null
         mainWindow = null
     })
