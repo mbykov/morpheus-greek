d57768c113a256db23d042715debe2095b3e066f
Michael Bykov <m.bykov@gmail.com>
2017-05-17 18:07:19 +0300

    book.icns

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ app.on('ready', () => {
     let platform = require('os').platform()
     let trayImage
     if (platform == 'darwin') {
-        trayImage = 'build/icon.icns'
+        trayImage = 'build/book.icns'
     }
     else if (platform == 'win32') {
         trayImage = 'build/icon.ico';
