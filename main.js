0050d843abf2d769cf9416e6c91078a2c125711c
Michael Bykov <m.bykov@gmail.com>
2017-06-21 22:55:21 +0300

    2.58

--- main.js
+++ main.js
@@ -93,7 +93,7 @@ function createWindow(msg) {
     // and load the index.html of the app.
     mainWindow.loadURL(`file://${__dirname}/index.html`)
     // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools()
     mainWindow.setAlwaysOnTop(true)
 
     mainWindow.webContents.on('did-finish-load', function() {
